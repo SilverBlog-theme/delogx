b731dc12317d703e4e8d2e3fd315a60e55f21abf
jianghua huang <qwe7002@hotmail.com>
2017-07-28 18:06:20 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ $(basename `pwd`) != "templates" ];then
+    echo "[Error] Please do this in the templates directory!"
+    exit
+fi
+if [ ! -d "delogx" ]; then
+    git clone https://github.com/SilverBlogTeam/delogx.git
+    cd delogx
+fi
+ln -s $(pwd)/static ../static/delogx
+if [ -f "config.json" ]; then
+    cp config.example.json config.json
+    vim config.json
+fi
